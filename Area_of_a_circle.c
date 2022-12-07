f16e46c9570eb7a798b99ccdce246ac9464faca3
patrunivarshini <119914370+patrunivarshini@users.noreply.github.com>
2022-12-07 09:42:02 +0530

    #include<stdio.h> void fun(int r) {      float a;     a=3.14*r*r;     printf("%0.2f",a); } int main() {     int r;     float a;     scanf("%d",&r);     fun(r); }

--- Area_of_a_circle.c
+++ Area_of_a_circle.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+void fun(int r)
+{
+     float a;
+    a=3.14*r*r;
+    printf("%0.2f",a);
+}
 int main()
 {
     int r;
     float a;
     scanf("%d",&r);
-    a=3.14*r*r;
-    printf("%0.2f",a);
+    fun(r);
 }
