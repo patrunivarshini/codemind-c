5eedf336cefa06b9800c51b50db6bd89fc046018
patrunivarshini <119914370+patrunivarshini@users.noreply.github.com>
2022-12-07 10:07:17 +0530

    #include<stdio.h> void fun(float a,float b) {     float c;     c=a*b;     printf("%0.2f",c); } int main() {     float a,b,c;     scanf("%f%f",&a,&b);     fun(a,b); }

--- Program_to_Multiply_Two_Floating-Point_Numbers.c
+++ Program_to_Multiply_Two_Floating-Point_Numbers.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+void fun(float a,float b)
+{
+    float c;
+    c=a*b;
+    printf("%0.2f",c);
+}
 int main()
 {
     float a,b,c;
     scanf("%f%f",&a,&b);
-    c=a*b;
-    printf("%0.2f",c);
+    fun(a,b);
 }
