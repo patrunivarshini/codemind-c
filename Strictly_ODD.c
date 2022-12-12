e10a22e455129bdbcacc17455f1b722cb0ce1142
patrunivarshini <119914370+patrunivarshini@users.noreply.github.com>
2022-12-12 11:48:00 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0&&i%2==0)         {             printf("False");             return 0;         }     }     printf("True"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0&&i%2==0)
+        {
+            printf("False");
+            return 0;
+        }
+    }
+    printf("True");
+}
