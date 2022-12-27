0b4883b426ec7a8ba8e6009518869f5acdda3018
patrunivarshini <119914370+patrunivarshini@users.noreply.github.com>
2022-12-27 11:57:19 +0530

    #include<stdio.h> void fun(int e,int m,int p,int c,int cs) {    if(e>=34&&m>=34&&p>=34&&c>=34&&cs>=34)     {         printf("PASSED");     }     else     {         printf("FAILED");     } } int main() {     int e,m,p,c,cs;     scanf("%d%d%d%d%d",&e,&m,&p,&c,&cs);     fun(e,m,p,c,cs); }

--- Passed_or_Failed.c
+++ Passed_or_Failed.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-int main()
+void fun(int e,int m,int p,int c,int cs)
 {
-    int e,m,p,c,cs;
-    scanf("%d%d%d%d%d",&e,&m,&p,&c,&cs);
-    if(e>=34&&m>=34&&p>=34&&c>=34&&cs>=34)
+   if(e>=34&&m>=34&&p>=34&&c>=34&&cs>=34)
     {
         printf("PASSED");
     }
     else
     {
         printf("FAILED");
-    }
+    } 
+}
+int main()
+{
+    int e,m,p,c,cs;
+    scanf("%d%d%d%d%d",&e,&m,&p,&c,&cs);
+    fun(e,m,p,c,cs);
 }
